af7647cb5296e1aac699a82d235b023a1d7f227a
AshleighWong <ashleightwong@gmail.com>
2024-10-23 13:08:16 -0400

    css stuff

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ import updateScrape from "./updateScrape";
 
 export default async function Home() {
 
-const url1 = "https://github.com/ouckah/Summer2025-Internships"
-const url2 = "https://github.com/SimplifyJobs/Summer2025-Internships"
+  const url1 = "https://github.com/ouckah/Summer2025-Internships"
+  const url2 = "https://github.com/SimplifyJobs/Summer2025-Internships"
 
 
-// updateScrape(url1);
-// setTimeout(() => {
-//   updateScrape(url2);
-// }, 10000); // 10000 milliseconds = 10 seconds
+  // updateScrape(url1);
+  // setTimeout(() => {
+  //   updateScrape(url2);
+  // }, 10000); // 10000 milliseconds = 10 seconds
 
-//uncomment when update ^
+  //uncomment when update ^
 
   async function InternshipsTable() {
     const internships = await getInternships(); // Fetch data from the server
@@ -25,7 +25,10 @@ const url2 = "https://github.com/SimplifyJobs/Summer2025-Internships"
 
   return (
     <>
-      <h1 className="my-4 text-center text-6xl font-bold text-gray-800 mt-20 mb-14">EzIntern</h1>
+      <div className="bg-slate-800 text-white text-center p-2">
+        <p className="text-lg font-semibold">Explore Top 2025 Internships!</p>
+      </div>
+      <h1 className="my-4 text-center text-6xl font-bold text-gray-800 mt-10 mb-14">EzIntern</h1>
       <div className="w-3/4 m-auto">
         <Suspense fallback={<div>Loading...</div>}>
           <InternshipsTable />
--- components/data-table.tsx
+++ components/data-table.tsx
@@ -39,7 +39,7 @@ interface DataTableProps<TData, TValue> {
 export function DataTable<TData, TValue>({
   columns,
   data,
-  maxHeight = "750px", // Default height if not specified
+  maxHeight = "760px", // Default height if not specified
 }: DataTableProps<TData, TValue>) {
   const [globalFilter, setGlobalFilter] = React.useState("")
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
@@ -97,7 +97,7 @@ export function DataTable<TData, TValue>({
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
-      <div className="rounded-md border border-black">
+      <div className="rounded-md border-1 border-black">
         <div style={{ maxHeight: maxHeight, overflow: 'auto' }}>
           <Table>
             <TableHeader className="sticky top-0 z-10">
